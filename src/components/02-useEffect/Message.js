321dd53da6ab39edaab9163ae87d8b343f17d3fe
mdCarron <mdcarron.dev@gmail.com>
2020-10-13 12:18:40 -0300

    Completa 7. useEffect unmount - Cleanup

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,16 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Messaje = () => {
+  const [{ x, y }, setCoors] = useState({ x: 0, y: 0 });
+
+  const mouseMove = (e) => {
+    const coors = { x: e.x, y: e.y };
+    setCoors(coors);
+  };
+
   useEffect(() => {
-    console.log("componente Message montado");
+    window.addEventListener("mousemove", mouseMove);
     return () => {
-      console.log("componente desmontado");
+      window.removeEventListener("mousemove", mouseMove);
     };
   }, []);
 
   return (
     <div>
       <h3>Mensaje desde Messaje.js</h3>
+      <p>
+        Coordenadas: X: {x} - Y: {y}
+      </p>
     </div>
   );
 };
