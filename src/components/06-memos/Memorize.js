fe8368df64c89e338a2a3a674e98a2fadb569e48
mdCarron <mdcarron.dev@gmail.com>
2020-10-16 20:43:39 -0300

    Completa 15. Memo - Método de React

---
+++ src/components/06-memos/Memorize.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { useCounter } from "../../hooks/useCounter";
+import Small from "./Small";
+
+const Memorize = () => {
+  const { counter, increment } = useCounter(10);
+  const [show, setShow] = useState(false);
+
+  return (
+    <div className="container mt-5">
+      <h1>Memorize</h1>
+      <hr />
+
+      <h2>
+        Counter: <Small value={counter} />
+      </h2>
+
+      <button
+        className="btn btn-success m-2"
+        onClick={() => {
+          increment();
+        }}
+      >
+        +1
+      </button>
+      <button
+        className="btn btn-outline-success m-2"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Show / Hide ({JSON.stringify(show)})
+      </button>
+    </div>
+  );
+};
+
+export default Memorize;
---
+++ src/components/06-memos/Small.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Small = React.memo(({ value }) => {
+  console.log("Small actualizado");
+  return <small>{value}</small>;
+});
+
+export default Small;
