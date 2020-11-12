bdccc400c5cd6e5609bb75359c1d2aca00bce053
mdCarron <mdcarron.dev@gmail.com>
2020-11-12 20:40:25 -0300

    Completa 6. Creando el cascaron de la lista de TODOs

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,10 +1,16 @@
 import React, { useReducer } from "react";
 import { todoReducer } from "./todoReducer";
+import "./TodoApp.css";
 
 const initialState = [
   {
     id: new Date().getTime(),
     description: "aprender react js",
+    done: false,
+  },
+  {
+    id: new Date().getTime(),
+    description: "terminar lección 11 de cero a experto",
     done: false,
   },
 ];
@@ -16,14 +22,41 @@ const TodoApp = () => {
 
   return (
     <div className="container">
-      <h1 className="mt-5">TodoApp</h1>
+      <h1 className="mt-5">TodoApp ({todos.length})</h1>
       <hr />
-
-      <ul>
-        <li>Mesclar masa madre</li>
-        <li>Cocinar pan</li>
-        <li>Comprar dulce de leche</li>
-      </ul>
+      <div className="row">
+        <div className="col-sm-12 col-md-4">
+          <h3>Agregar Tarea</h3>
+          <hr />
+          <form>
+            <input
+              type="text"
+              name="description"
+              placeholder="Nueva Tarea..."
+              autoComplete="off"
+              className="form-control my-2"
+            />
+            <button className="btn btn-outline-success btn-block my-2">
+              Agregar
+            </button>
+          </form>
+        </div>
+        <div className="col-sm-12 col-md-8">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => (
+              <li
+                key={todo.id}
+                className="list-group-item d-flex justify-content-between align-items-center"
+              >
+                <p className="">
+                  <b>{i + 1}.</b> {todo.description}
+                </p>
+                <button className="btn btn-danger">Borrar</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
