2f0dedc54d7e18c88169fba59ae8fb6d41748360
mdCarron <mdcarron.dev@gmail.com>
2020-11-28 14:10:52 -0300

    Completa 11. Pruebas restantes de mi Reducer

--- src/tests/components/08-useReducer/todoReducer.test.js
+++ src/tests/components/08-useReducer/todoReducer.test.js
@@ -7,7 +7,7 @@ describe("todoReducer.js", () => {
     expect(state).toEqual(demoTodos);
   });
 
-  test("should return new todo", () => {
+  test("should return NEW todo", () => {
     const newTodo = { id: 3, description: "Aprender Node.js", done: false };
     const newAction = {
       type: "add",
@@ -17,5 +17,26 @@ describe("todoReducer.js", () => {
 
     expect(state.length).toBe(3);
     expect(state).toEqual([...demoTodos, newTodo]);
+  });
+
+  test("should return new state without DELETED todo", () => {
+    const action = {
+      type: "delete",
+      payload: 2,
+    };
+    const state = todoReducer(demoTodos, action);
+
+    expect(state.length).toBe(1);
+    expect(state).toEqual([demoTodos[0]]);
+  });
+
+  test("should return todo with done TOGGLE ", () => {
+    const action = {
+      type: "toggle",
+      payload: 2,
+    };
+    const state = todoReducer(demoTodos, action);
+    expect(state[1].done).toBe(true);
+    expect(state[0]).toEqual(demoTodos[0]);
   });
 });
--- src/tests/fixtures/demoTodos.js
+++ src/tests/fixtures/demoTodos.js
@@ -1,4 +1,4 @@
 export const demoTodos = [
   { id: 1, description: "Aprender React", done: false },
-  { id: 1, description: "Aprender MongoDB", done: false },
+  { id: 2, description: "Aprender MongoDB", done: false },
 ];
